0a277e6d819f30d06767a3a7eb4387aa5b69d1bc
PavelVe <vejnar.p@gmail.com>
2025-06-22 14:40:03 +0200

    add user server

--- TeltonikaServer/test/test_server_from_client.py
+++ TeltonikaServer/test/test_server_from_client.py
@@ -6,7 +6,7 @@ data = b'\x00\x00\x00\x00\x00\x00\x00\x00\x01'
 payload = len(imei).to_bytes(2, 'big') + imei + data
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect(("192.168.1.161", 3030))
+    s.connect(("0.0.0.0", 3031))
     s.sendall(payload)
     response = s.recv(16)
     print("ACK:", response.hex())
